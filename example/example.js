2f052d1406f568029f651929c283f933234c87da
Josh <c13joshua.christman@gmail.com>
2015-01-28 15:31:43 -0500

    - Full functionality

--- example/example.js
+++ example/example.js
@@ -11,25 +11,28 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
     Template.body.helpers({
-        setColumnData : function() {
+        setup : function() {
             var _columns = [
                 {
                     name : 'Row Number',
                     varName : 'rowNum',
-                    class : 'row-number'
+                    class : 'row-number custom-td'
                 },
                 {
                     name : 'Random Data 1',
                     varName : 'data_1',
-                    class : 'data-1'
+                    class : 'data-1 custom-td'
                 },
                 {
                     name : 'Random Data 2',
                     varName : 'data_2',
-                    class : 'data-2'
+                    class : 'data-2 custom-td'
                 }
             ];
-            return _.extend(this, { columns : _columns });
+            var _css = {
+                row_class : 'custom-row'
+            };
+            return _.extend(this, { columns : _columns , css : _css });
         }
     });
 }
