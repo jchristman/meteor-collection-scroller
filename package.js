dd8eb341883faaca90d24e7c3129f901faddaf49
Josh <c13joshua.christman@gmail.com>
2015-01-30 13:50:52 -0500

    - Fixed an issue where the table top moved prior to the new data being   ready. This caused a distracting jump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'jchristman:collection-scroller',
   summary: 'Scroll a *large* collection as if it were all downloaded at once',
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/jchristman/meteor-collection-scroller'
 });
 
