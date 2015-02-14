6df5a341c695a0f440b9602f19cc5bed1988b7f1
Josh <c13joshua.christman@gmail.com>
2015-02-13 23:54:51 -0500

    - Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'jchristman:collection-scroller',
   summary: 'Scroll a *large* collection as if it were all downloaded at once',
-  version: '1.0.6_3',
+  version: '1.0.6_4',
   git: 'https://github.com/jchristman/meteor-collection-scroller'
 });
 
